b6e79f59e3c63a57dc5dd8794f9c0f87fba90b6f
copiousamounts <nick.hernstedt@gmail.com>
2020-02-03 13:02:03 -0800

    created variables for current time slot

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,11 @@
-let currentDate = moment();
+// get time elements
 
-    console.log(currentDate.toString());
+var nineEL = document.getElementById("9AM");
+var nineEL = document.getElementById("10AM");
+var nineEL = document.getElementById("11AM");
+var nineEL = document.getElementById("12PM");
+var nineEL = document.getElementById("1PM");
+var nineEL = document.getElementById("2PM");
+var nineEL = document.getElementById("3PM");
+var nineEL = document.getElementById("4PM");
+var nineEL = document.getElementById("5PM");
