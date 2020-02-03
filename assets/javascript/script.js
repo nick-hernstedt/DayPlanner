a12630ced1ac580237945a8a5a52385912f05229
copiousamounts <nick.hernstedt@gmail.com>
2020-02-03 13:16:44 -0800

    added click function to save to local storage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -21,3 +21,55 @@ var nineSaveEl = docuement.getElementById("2save");
 var nineSaveEl = docuement.getElementById("3save");
 var nineSaveEl = docuement.getElementById("4save");
 var nineSaveEl = docuement.getElementById("5save");
+
+//setting current date to show
+$(`#currentDay`).text(moment().format("MMMM Do YYYY, h:mm a"));
+
+//setting schedule values
+nineEL.value = localStorage.getItem("nine");
+tenEl.value = localStorage.getItem("ten");
+elevenEl.value = localStorage.getItem("eleven");
+twelveEl.value = localStorage.getItem("twelve");
+oneEl.value = localStorage.getItem("one");
+twoEl.value = localStorage.getItem("two");
+threeEl.value = localStorage.getItem("three");
+fourEl.value = localStorage.getItem("four");
+fiveEl.value = localStorage.getItem("five");
+
+//when button is clicked save to local storage
+
+nineSaveEl.addEventListener("click", function() {
+  localStorage.setItem("nine", nineEl.value);
+});
+
+tenSaveEl.addEventListener("click", function() {
+  localStorage.setItem("ten", tenEl.value);
+});
+
+elevenSaveEl.addEventListener("click", function() {
+  localStorage.setItem("eleven", elevenEl.value);
+});
+
+twelveSaveEl.addEventListener("click", function() {
+  localStorage.setItem("twelve", twelveEl.value);
+});
+
+oneSaveEl.addEventListener("click", function() {
+  localStorage.setItem("one", oneEl.value);
+});
+
+twoSaveEl.addEventListener("click", function() {
+  localStorage.setItem("two", twoEl.value);
+});
+
+threeSaveEl.addEventListener("click", function() {
+  localStorage.setItem("three", threeEl.value);
+});
+
+fourSaveEl.addEventListener("click", function() {
+  localStorage.setItem("four", fourEl.value);
+});
+
+fiveSaveEl.addEventListener("click", function() {
+  localStorage.setItem("five", fiveEl.value);
+});
