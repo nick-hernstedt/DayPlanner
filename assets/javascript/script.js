b1fa0542c2384b4906223aaf915358f41ecad2f5
copiousamounts <nick.hernstedt@gmail.com>
2020-02-03 13:04:31 -0800

    added variables for save slots

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,3 +9,15 @@ var nineEL = document.getElementById("2PM");
 var nineEL = document.getElementById("3PM");
 var nineEL = document.getElementById("4PM");
 var nineEL = document.getElementById("5PM");
+
+// save slot variables
+
+var nineSaveEl = docuement.getElementById("9save");
+var nineSaveEl = docuement.getElementById("10save");
+var nineSaveEl = docuement.getElementById("11save");
+var nineSaveEl = docuement.getElementById("12save");
+var nineSaveEl = docuement.getElementById("1save");
+var nineSaveEl = docuement.getElementById("2save");
+var nineSaveEl = docuement.getElementById("3save");
+var nineSaveEl = docuement.getElementById("4save");
+var nineSaveEl = docuement.getElementById("5save");
