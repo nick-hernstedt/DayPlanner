4c5e27e082847723609aed534da1d6a1e2118cae
copiousamounts <nick.hernstedt@gmail.com>
2020-02-04 18:30:16 -0800

    changed functions that change color

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -77,167 +77,257 @@ fiveSaveEl.addEventListener(`click`, function() {
 
 //making colours change depending on time
 
-if (moment().hour() < 9) {
-  nineEL.classList.add(`future`);
-  tenEL.classList.add(`future`);
-  elevenEL.classList.add(`future`);
-  twelveEL.classList.add(`future`);
-  oneEL.classList.add(`future`);
-  twoEL.classList.add(`future`);
-  threeEL.classList.add(`future`);
-  fourEL.classList.add(`future`);
-  fiveEL.classList.add(`future`);
-} else if (moment().hour() == 9) {
-  nineEL.classList.add(`present`);
-  tenEL.classList.add(`future`);
-  elevenEL.classList.add(`future`);
-  twelveEL.classList.add(`future`);
-  oneEL.classList.add(`future`);
-  twoEL.classList.add(`future`);
-  threeEL.classList.add(`future`);
-  fourEL.classList.add(`future`);
-  fiveEL.classList.add(`future`);
-} else if (moment().hour() == 10) {
-  nineEL.classList.add(`past`);
-  nineEL.classList.remove(`future`);
-  tenEL.classList.add(`present`);
-  tenEL.classList.remove(`future`);
-  elevenEL.classList.add(`future`);
-  twelveEL.classList.add(`future`);
-  oneEL.classList.add(`future`);
-  twoEL.classList.add(`future`);
-  threeEL.classList.add(`future`);
-  fourEL.classList.add(`future`);
-  fiveEL.classList.add(`future`);
-} else if (moment().hour() == 11) {
-  nineEL.classList.add(`past`);
-  nineEL.classList.remove(`future`);
-  tenEL.classList.add(`past`);
-  tenEL.classList.remove(`future`);
-  elevenEL.classList.add(`present`);
-  elevenEL.classList.remove(`future`);
-  twelveEL.classList.add(`future`);
-  oneEL.classList.add(`future`);
-  twoEL.classList.add(`future`);
-  threeEL.classList.add(`future`);
-  fourEL.classList.add(`future`);
-  fiveEL.classList.add(`future`);
-} else if (moment().hour() == 12) {
-  nineEL.classList.add(`past`);
-  nineEL.classList.remove(`future`);
-  tenEL.classList.add(`past`);
-  tenEL.classList.remove(`future`);
-  elevenEL.classList.add(`past`);
-  elevenEL.classList.remove(`future`);
-  twelveEL.classList.add(`present`);
-  twelveEL.classList.remove(`future`);
-  oneEL.classList.add(`future`);
-  twoEL.classList.add(`future`);
-  threeEL.classList.add(`future`);
-  fourEL.classList.add(`future`);
-  fiveEL.classList.add(`future`);
-} else if (moment().hour() == 13) {
-  nineEL.classList.add(`past`);
-  nineEL.classList.remove(`future`);
-  tenEL.classList.add(`past`);
-  tenEL.classList.remove(`future`);
-  elevenEL.classList.add(`past`);
-  elevenEL.classList.remove(`future`);
-  twelveEL.classList.add(`past`);
-  twelveEL.classList.remove(`future`);
-  oneEL.classList.add(`present`);
-  oneEL.classList.remove(`future`);
-  twoEL.classList.add(`future`);
-  threeEL.classList.add(`future`);
-  fourEL.classList.add(`future`);
-  fiveEL.classList.add(`future`);
-} else if (moment().hour() == 14) {
-  nineEL.classList.add(`past`);
-  nineEL.classList.remove(`future`);
-  tenEL.classList.add(`past`);
-  tenEL.classList.remove(`future`);
-  elevenEL.classList.add(`past`);
-  elevenEL.classList.remove(`future`);
-  twelveEL.classList.add(`past`);
-  twelveEL.classList.remove(`future`);
-  oneEL.classList.add(`past`);
-  oneEL.classList.remove(`future`);
-  twoEL.classList.add(`present`);
-  twoEL.classList.remove(`future`);
-  threeEL.classList.add(`future`);
-  fourEL.classList.add(`future`);
-  fiveEL.classList.add(`future`);
-} else if (moment().hour() == 15) {
-  nineEL.classList.add(`past`);
-  nineEL.classList.remove(`future`);
-  tenEL.classList.add(`past`);
-  tenEL.classList.remove(`future`);
-  elevenEL.classList.add(`past`);
-  elevenEL.classList.remove(`future`);
-  twelveEL.classList.add(`past`);
-  twelveEL.classList.remove(`future`);
-  oneEL.classList.add(`past`);
-  oneEL.classList.remove(`future`);
-  twoEL.classList.add(`past`);
-  twoEL.classList.remove(`future`);
-  threeEL.classList.add(`present`);
-  threeEL.classList.remove(`future`);
-  fourEL.classList.add(`future`);
-  fiveEL.classList.add(`future`);
-} else if (moment().hour() == 16) {
-  nineEL.classList.add(`past`);
-  nineEL.classList.remove(`future`);
-  tenEL.classList.add(`past`);
-  tenEL.classList.remove(`future`);
-  elevenEL.classList.add(`past`);
-  elevenEL.classList.remove(`future`);
-  twelveEL.classList.add(`past`);
-  twelveEL.classList.remove(`future`);
-  oneEL.classList.add(`past`);
-  oneEL.classList.remove(`future`);
-  twoEL.classList.add(`past`);
-  twoEL.classList.remove(`future`);
-  threeEL.classList.add(`past`);
-  threeEL.classList.remove(`future`);
-  fourEL.classList.add(`present`);
-  fourEL.classList.remove(`future`);
-  fiveEL.classList.add(`future`);
-} else if (moment().hour() == 17) {
-  nineEL.classList.add(`past`);
-  nineEL.classList.remove(`future`);
-  tenEL.classList.add(`past`);
-  tenEL.classList.remove(`future`);
-  elevenEL.classList.add(`past`);
-  elevenEL.classList.remove(`future`);
-  twelveEL.classList.add(`past`);
-  twelveEL.classList.remove(`future`);
-  oneEL.classList.add(`past`);
-  oneEL.classList.remove(`future`);
-  twoEL.classList.add(`past`);
-  twoEL.classList.remove(`future`);
-  threeEL.classList.add(`past`);
-  threeEL.classList.remove(`future`);
-  fourEL.classList.add(`past`);
-  fourEL.classList.remove(`future`);
-  fiveEL.classList.add(`present`);
-  fiveEL.classList.remove(`future`);
-} else {
-  nineEL.classList.add(`past`);
-  nineEL.classList.remove(`future`);
-  tenEL.classList.add(`past`);
-  tenEL.classList.remove(`future`);
-  elevenEL.classList.add(`past`);
-  elevenEL.classList.remove(`future`);
-  twelveEL.classList.add(`past`);
-  twelveEL.classList.remove(`future`);
-  oneEL.classList.add(`past`);
-  oneEL.classList.remove(`future`);
-  twoEL.classList.add(`past`);
-  twoEL.classList.remove(`future`);
-  threeEL.classList.add(`past`);
-  threeEL.classList.remove(`future`);
-  fourEL.classList.add(`past`);
-  fourEL.classList.remove(`future`);
-  fiveEL.classList.add(`past`);
-  fiveEL.classList.remove(`future`);
+// var for color change
+var now = moment().format("H")
+
+//display time on page
+var display = moment().format('dddd, MMMM Do YYYY, h:mm a')
+
+$("#currentDay").html(display)
+
+///// COLOR DEPENDING ON TIME//////
+// 9 am
+if (now > 9) {
+    nineEL.classList.add("past")
+    } else if (now == 9) {
+    nineEL.classList.add("present")
+        } else {
+    nineEL.classList.add("futute")
 }
+
+// 10 am
+if (now > 10) {
+    tenEL.classList.add("past")
+    } else if (now == 10) {
+    tenEL.classList.add("present")
+        } else {
+    tenEL.classList.add("future")
+}
+
+// 11 am
+if (now > 11) {
+    elevenEL.classList.add("past")
+    } else if (now == 11) {
+    elevenEL.classList.add("present")
+        } else {
+    elevenEL.classList.add("future")
+}
+
+// 12 pm
+if (now > 12) {
+    twelveEL.classList.add("past")
+    } else if (now == 12) {
+    twelveEL.classList.add("present")
+        } else {
+    twelveEL.classList.add("future")
+}
+
+// 1 pm
+if (now > 13) {
+    oneEL.classList.add("past")
+    } else if (now == 13) {
+    oneEL.classList.add("present")
+        } else {
+    oneEL.classList.add("future")
+}
+
+// 2 pm
+if (now > 14) {
+    twoEL.classList.add("past")
+    } else if (now == 14) {
+    twoEL.classList.add("present")
+        } else {
+    twoEL.classList.add("future")
+}
+
+// 3 pm
+if (now > 15) {
+    threeEL.classList.add("past")
+    } else if (now == 15) {
+    threeEL.classList.add("present")
+        } else {
+    threeEL.classList.add("future")
+}
+
+// 4 pm
+if (now > 16) {
+    fourEL.classList.add("past")
+    } else if (now == 16) {
+    fourEL.classList.add("present")
+        } else {
+    fourEL.classList.add("future")
+}
+
+// 5 pm
+if (now > 17) {
+    fiveEL.classList.add("past")
+    } else if (now == 17) {
+    fiveEL.classList.add("present")
+        } else {
+    fiveEL.classList.add("future")
+}
+
+// if (moment().hour() < 9) {
+//   nineEL.classList.add(`future`);
+//   tenEL.classList.add(`future`);
+//   elevenEL.classList.add(`future`);
+//   twelveEL.classList.add(`future`);
+//   oneEL.classList.add(`future`);
+//   twoEL.classList.add(`future`);
+//   threeEL.classList.add(`future`);
+//   fourEL.classList.add(`future`);
+//   fiveEL.classList.add(`future`);
+// } else if (moment().hour() == 9) {
+//   nineEL.classList.add(`present`);
+//   tenEL.classList.add(`future`);
+//   elevenEL.classList.add(`future`);
+//   twelveEL.classList.add(`future`);
+//   oneEL.classList.add(`future`);
+//   twoEL.classList.add(`future`);
+//   threeEL.classList.add(`future`);
+//   fourEL.classList.add(`future`);
+//   fiveEL.classList.add(`future`);
+// } else if (moment().hour() == 10) {
+//   nineEL.classList.add(`past`);
+//   nineEL.classList.remove(`future`);
+//   tenEL.classList.add(`present`);
+//   tenEL.classList.remove(`future`);
+//   elevenEL.classList.add(`future`);
+//   twelveEL.classList.add(`future`);
+//   oneEL.classList.add(`future`);
+//   twoEL.classList.add(`future`);
+//   threeEL.classList.add(`future`);
+//   fourEL.classList.add(`future`);
+//   fiveEL.classList.add(`future`);
+// } else if (moment().hour() == 11) {
+//   nineEL.classList.add(`past`);
+//   nineEL.classList.remove(`future`);
+//   tenEL.classList.add(`past`);
+//   tenEL.classList.remove(`future`);
+//   elevenEL.classList.add(`present`);
+//   elevenEL.classList.remove(`future`);
+//   twelveEL.classList.add(`future`);
+//   oneEL.classList.add(`future`);
+//   twoEL.classList.add(`future`);
+//   threeEL.classList.add(`future`);
+//   fourEL.classList.add(`future`);
+//   fiveEL.classList.add(`future`);
+// } else if (moment().hour() == 12) {
+//   nineEL.classList.add(`past`);
+//   nineEL.classList.remove(`future`);
+//   tenEL.classList.add(`past`);
+//   tenEL.classList.remove(`future`);
+//   elevenEL.classList.add(`past`);
+//   elevenEL.classList.remove(`future`);
+//   twelveEL.classList.add(`present`);
+//   twelveEL.classList.remove(`future`);
+//   oneEL.classList.add(`future`);
+//   twoEL.classList.add(`future`);
+//   threeEL.classList.add(`future`);
+//   fourEL.classList.add(`future`);
+//   fiveEL.classList.add(`future`);
+// } else if (moment().hour() == 13) {
+//   nineEL.classList.add(`past`);
+//   nineEL.classList.remove(`future`);
+//   tenEL.classList.add(`past`);
+//   tenEL.classList.remove(`future`);
+//   elevenEL.classList.add(`past`);
+//   elevenEL.classList.remove(`future`);
+//   twelveEL.classList.add(`past`);
+//   twelveEL.classList.remove(`future`);
+//   oneEL.classList.add(`present`);
+//   oneEL.classList.remove(`future`);
+//   twoEL.classList.add(`future`);
+//   threeEL.classList.add(`future`);
+//   fourEL.classList.add(`future`);
+//   fiveEL.classList.add(`future`);
+// } else if (moment().hour() == 14) {
+//   nineEL.classList.add(`past`);
+//   nineEL.classList.remove(`future`);
+//   tenEL.classList.add(`past`);
+//   tenEL.classList.remove(`future`);
+//   elevenEL.classList.add(`past`);
+//   elevenEL.classList.remove(`future`);
+//   twelveEL.classList.add(`past`);
+//   twelveEL.classList.remove(`future`);
+//   oneEL.classList.add(`past`);
+//   oneEL.classList.remove(`future`);
+//   twoEL.classList.add(`present`);
+//   twoEL.classList.remove(`future`);
+//   threeEL.classList.add(`future`);
+//   fourEL.classList.add(`future`);
+//   fiveEL.classList.add(`future`);
+// } else if (moment().hour() == 15) {
+//   nineEL.classList.add(`past`);
+//   nineEL.classList.remove(`future`);
+//   tenEL.classList.add(`past`);
+//   tenEL.classList.remove(`future`);
+//   elevenEL.classList.add(`past`);
+//   elevenEL.classList.remove(`future`);
+//   twelveEL.classList.add(`past`);
+//   twelveEL.classList.remove(`future`);
+//   oneEL.classList.add(`past`);
+//   oneEL.classList.remove(`future`);
+//   twoEL.classList.add(`past`);
+//   twoEL.classList.remove(`future`);
+//   threeEL.classList.add(`present`);
+//   threeEL.classList.remove(`future`);
+//   fourEL.classList.add(`future`);
+//   fiveEL.classList.add(`future`);
+// } else if (moment().hour() == 16) {
+//   nineEL.classList.add(`past`);
+//   nineEL.classList.remove(`future`);
+//   tenEL.classList.add(`past`);
+//   tenEL.classList.remove(`future`);
+//   elevenEL.classList.add(`past`);
+//   elevenEL.classList.remove(`future`);
+//   twelveEL.classList.add(`past`);
+//   twelveEL.classList.remove(`future`);
+//   oneEL.classList.add(`past`);
+//   oneEL.classList.remove(`future`);
+//   twoEL.classList.add(`past`);
+//   twoEL.classList.remove(`future`);
+//   threeEL.classList.add(`past`);
+//   threeEL.classList.remove(`future`);
+//   fourEL.classList.add(`present`);
+//   fourEL.classList.remove(`future`);
+//   fiveEL.classList.add(`future`);
+// } else if (moment().hour() == 17) {
+//   nineEL.classList.add(`past`);
+//   nineEL.classList.remove(`future`);
+//   tenEL.classList.add(`past`);
+//   tenEL.classList.remove(`future`);
+//   elevenEL.classList.add(`past`);
+//   elevenEL.classList.remove(`future`);
+//   twelveEL.classList.add(`past`);
+//   twelveEL.classList.remove(`future`);
+//   oneEL.classList.add(`past`);
+//   oneEL.classList.remove(`future`);
+//   twoEL.classList.add(`past`);
+//   twoEL.classList.remove(`future`);
+//   threeEL.classList.add(`past`);
+//   threeEL.classList.remove(`future`);
+//   fourEL.classList.add(`past`);
+//   fourEL.classList.remove(`future`);
+//   fiveEL.classList.add(`present`);
+//   fiveEL.classList.remove(`future`);
+// } else {
+//   nineEL.classList.add(`past`);
+//   nineEL.classList.remove(`future`);
+//   tenEL.classList.add(`past`);
+//   tenEL.classList.remove(`future`);
+//   elevenEL.classList.add(`past`);
+//   elevenEL.classList.remove(`future`);
+//   twelveEL.classList.add(`past`);
+//   twelveEL.classList.remove(`future`);
+//   oneEL.classList.add(`past`);
+//   oneEL.classList.remove(`future`);
+//   twoEL.classList.add(`past`);
+//   twoEL.classList.remove(`future`);
+//   threeEL.classList.add(`past`);
+//   threeEL.classList.remove(`future`);
+//   fourEL.classList.add(`past`);
+//   fourEL.classList.remove(`future`);
+//   fiveEL.classList.add(`past`);
+//   fiveEL.classList.remove(`future`);
+// }
